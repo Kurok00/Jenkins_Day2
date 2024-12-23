ac379ead0d3f6804eb40e7d9db9654c3489e40b3
Kurok00 <heneken96@gmail.com>
2024-12-23 14:43:21 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, world!",
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	if err := r.Run(":4000"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
